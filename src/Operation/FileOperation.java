fa6a6379fd4d9fb096703c4807cd989af5687ad2
oatzpure <oatsumetee@gmail.com>
2015-05-24 18:13:58 +0700

    OS_model+operation

---
+++ src/Operation/FileOperation.java
@@ -0,0 +1,36 @@
+package Operation;
+
+import java.io.File;
+
+public class FileOperation {
+	public void createfile(String filename){
+		
+	}
+	public void deletefile(String filename){
+		
+	}
+	public void openfile(String filename){
+		
+	}
+	public void closefile(String filename){
+		
+	}
+	public void readfile(String filename, int size){
+		
+	}
+	public void writefile(String filename, int size){
+		
+	}
+	public void seekfile(String filename, int start_block){
+		
+	}
+	public void linkfile(String old_filename, String new_filename){
+		
+	}
+	public int getsize(String filename){
+		return 0;
+	}
+	public int getoffset(String filename){
+		return 0;
+	}
+}
---
+++ src/Operation/FileSystemOperation.java
@@ -0,0 +1,15 @@
+package Operation;
+
+import java.io.File;
+
+public class FileSystemOperation {
+	public void initfilesystem(File fs){
+		
+	}
+	public void consistencycheck(File fs){
+		
+	}
+	public void efficiencycheck(File fs){
+		
+	}
+}
